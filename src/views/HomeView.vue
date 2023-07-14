fe6a3136f7ea81e3c69dd1ae6178790025dd7503
zhujiruo <zhujiruo@foxmail.com>
2023-07-14 15:42:27 +0800

    ✨ feat:update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,11 +4,11 @@ import AutoTable from "@/components/AutoTable.vue";
 import { useRouter } from "vue-router";
 // import { onMounted } from "vue";
 import { inject } from "vue";
-import {closeSocket,createSocket} from "../hooks/useWebSocket";
-const onMessageList:any = inject("onMessageList"); // 接收注入
-const socketMessage = (res:any) => {
-    // const data = JSON.parse(res.data);
-    // do something
+import { closeSocket, createSocket } from "../hooks/useWebSocket";
+const onMessageList: any = inject("onMessageList"); // 接收注入
+const socketMessage = (res: any) => {
+  // const data = JSON.parse(res.data);
+  // do something
 };
 onMessageList?.push(socketMessage); // 将自定义处理事件插入onMessge集合
 
@@ -17,7 +17,6 @@ const router = useRouter();
 </script>
 
 <template>
- 
   <el-tooltip placement="left" effect="light">
     <template #content>
       <div style="width: 190px; height: 85px; padding: 10px 6px">
@@ -35,19 +34,27 @@ const router = useRouter();
     </div>
   </el-tooltip>
   <el-tooltip placement="left" content="返回顶部" effect="light">
-    <el-backtop right="50" bottom="200" visibilityHeight="40" class="back" />
+    <!-- <el-backtop right="{50}" bottom="200" visibilityHeight="40" class="back" /> -->
   </el-tooltip>
   <main>
     <button @click="createSocket">开启连接</button>
-  <button @click="closeSocket">关闭连接</button>
+    <button @click="closeSocket">关闭连接</button>
     <div class="main" @click="router.push({ name: 'partner' })" />
     <HomeCard />
     <!-- <AutoTable /> -->
-    <div style="height: 1000px"></div>
+    <div style="height: 1000px">
+      <div class="ex">123123</div>
+      <a class="ex">111</a>
+      <a class="ea">24234</a>
+    </div>
   </main>
 </template>
 
 <style scoped lang="less">
+a .ex {
+  color: red;
+}
+
 .contact {
   background-color: #fff;
   position: fixed;
